d058addf4739c71d855db87f80d8d2d998d28fc5
Toshine <toshineluv99@yahoo.com>
2020-04-14 01:39:11 +0100

    project setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const router = require("./router");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.set("views", "pages");
+app.set("view engine", "ejs");
+
+app.use("/", router);
+
+app.listen(3000);
