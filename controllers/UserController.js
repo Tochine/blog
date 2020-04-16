2a3a24069be696e131d5c49ff52a2a9b411320dc
Toshine <toshineluv99@yahoo.com>
2020-04-17 00:29:23 +0100

    adding create-post feature

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,6 +20,17 @@ exports.register = function (req, res) {
     });
 };
 
+exports.mustBeLoggedIn = function (req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash("errors", "You must be logged in to perform that action");
+    req.session.save(function () {
+      res.redirect("/");
+    });
+  }
+};
+
 exports.login = function (req, res) {
   let user = new User(req.body);
   user
@@ -46,10 +57,7 @@ exports.logout = function (req, res) {
 
 exports.home = function (req, res) {
   if (req.session.user) {
-    res.render("home-dashboard", {
-      username: req.session.user.username,
-      avatar: req.session.user.avatar,
-    });
+    res.render("home-dashboard");
   } else {
     res.render("home-guest", {
       errors: req.flash("errors"),
--- router.js
+++ router.js
@@ -3,10 +3,19 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("./controllers/UserController");
+const postController = require("./controllers/PostController");
 
+// User related routes
 router.get("/", userController.home);
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
+
+// Post related routes
+router.get(
+  "/create-post",
+  userController.mustBeLoggedIn,
+  postController.viewCreateScreen
+);
 
 module.exports = router;
