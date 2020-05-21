141b0634a78c207a8eb77bb4cbfddb32cbb4279b
Toshine <toshineluv99@yahoo.com>
2020-05-22 00:31:56 +0100

    sanitizing html on the frontend

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import DOMPurify from "dompurify";
 
 export default class Search {
   // 1. Select DOM elements and keep track of any useful data
@@ -60,7 +61,7 @@ export default class Search {
 
   renderResultsHTML(posts) {
     if (posts.length) {
-      this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
+      this.resultsArea.innerHTML = DOMPurify.sanitize(`<div class="list-group shadow-sm">
       <div class="list-group-item active"><strong>Search Results</strong> (${
         posts.length > 1 ? `${posts.length} items found` : "1 item found"
       })</div>
@@ -81,7 +82,7 @@ export default class Search {
           })
           .join("")}
       
-      </div>`;
+      </div>`);
     } else {
       this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">No results found for that search.</p>`;
     }
